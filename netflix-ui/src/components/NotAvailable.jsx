7e4c813fec96f663a43b456cc0c6b2c8657e31d5
evandrotvc <evandro-tvc@hotmail.com>
2023-03-04 18:36:04 -0300

    fix: text in description

--- netflix-ui/src/components/NotAvailable.jsx
+++ netflix-ui/src/components/NotAvailable.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function NotAvailable() {
   return (
     <h1 className="not-available">
-      No Movies avaialble for the selected genre. Please select a different
+      No Movies avaliable for the selected genre. Please select a different
       genre.
     </h1>
   );
