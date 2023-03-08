003d74e3620b57618954f3a392fb8d986138384f
evandrotvc <evandro-tvc@hotmail.com>
2023-03-08 11:09:05 -0300

    feat: added cors in application in api. working comunication between front and back

--- netflix-api/config/application.rb
+++ netflix-api/config/application.rb
@@ -10,6 +10,9 @@ module NetflixApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    # config.web_console.whiny_requests = false
+    config.api_only = true
+
 
     # Configuration for the application, engines, and railties goes here.
     #
